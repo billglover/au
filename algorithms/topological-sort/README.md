35d5abe066287fa796d4c187682144398315d462
Bill Glover <bill@billglover.co.uk>
2018-09-20 23:26:39 +0100

    New Algorithm: Topological Sort (#56)
    * New Algorithm: Topological Sort
    Fixes #47

--- algorithms/topological-sort/README.md
+++ algorithms/topological-sort/README.md
@@ -34,8 +34,23 @@
 
 **Benchmark:**
 
-Analysis not yet complete.
+The benchmark results show how the running time of the Topological Sort algorithm varies with increasing node count.
+
+```plain
+$ go test -run=X -bench=. -benchmem
+goos: darwin
+goarch: amd64
+pkg: github.com/billglover/au/algorithms/topological-sort
+BenchmarkTopologicalSort/_10-4           2000000               630 ns/op             480 B/op         12 allocs/op
+BenchmarkTopologicalSort/_12-4           2000000               655 ns/op             560 B/op         13 allocs/op
+BenchmarkTopologicalSort/_14-4           2000000               683 ns/op             608 B/op         13 allocs/op
+BenchmarkTopologicalSort/_16-4           2000000               756 ns/op             640 B/op         14 allocs/op
+BenchmarkTopologicalSort/_18-4           2000000               919 ns/op             944 B/op         15 allocs/op
+BenchmarkTopologicalSort/_20-4           1000000              1032 ns/op             992 B/op         15 allocs/op
+PASS
+ok      github.com/billglover/au/algorithms/topological-sort    12.067s
+```
 
 **Complexity:**
 
-Analysis not yet complete.
+![Time Complexity: Topoloical Sort, increasing number of nodes](img/complexity_time.png)
--- algorithms/topological-sort/topological-sort_test.go
+++ algorithms/topological-sort/topological-sort_test.go
@@ -1,6 +1,7 @@
 package topologicalsort
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -22,6 +23,120 @@ var g1 = Graph{
 	[]Node{},
 }
 
+// Nodes: 10, Edges: 15
+var g10 = Graph{
+	[]Node{4, 6, 9},
+	[]Node{3, 6, 7, 8},
+	[]Node{4, 9},
+	[]Node{7, 9},
+	[]Node{6},
+	[]Node{6, 8},
+	[]Node{9},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
+// Nodes: 12, Edges: 22
+var g12 = Graph{
+	[]Node{4, 6, 9, 10},
+	[]Node{3, 6, 7, 8},
+	[]Node{4, 9, 10},
+	[]Node{7, 9, 10},
+	[]Node{6, 10, 11},
+	[]Node{6, 8, 11},
+	[]Node{9},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
+// Nodes: 14, Edges: 25
+var g14 = Graph{
+	[]Node{4, 6, 9, 10},
+	[]Node{3, 6, 7, 8, 12},
+	[]Node{4, 9, 10},
+	[]Node{7, 9, 10},
+	[]Node{6, 10, 11},
+	[]Node{6, 8, 11},
+	[]Node{9},
+	[]Node{12},
+	[]Node{11},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
+// Nodes: 16, Edges: 33
+var g16 = Graph{
+	[]Node{4, 6, 9, 10, 15},
+	[]Node{3, 6, 7, 8, 12, 14},
+	[]Node{4, 9, 10, 14, 15},
+	[]Node{7, 9, 10},
+	[]Node{6, 10, 11},
+	[]Node{6, 8, 11},
+	[]Node{9, 15},
+	[]Node{12},
+	[]Node{11},
+	[]Node{15},
+	[]Node{12, 14, 15},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
+// Nodes: 18, Edges: 43
+var g18 = Graph{
+	[]Node{4, 6, 9, 10, 15, 16},
+	[]Node{3, 6, 7, 8, 12, 14, 16},
+	[]Node{4, 9, 10, 14, 15, 16, 17},
+	[]Node{7, 9, 10},
+	[]Node{6, 10, 11, 17},
+	[]Node{6, 8, 11},
+	[]Node{9, 15, 17},
+	[]Node{12, 17},
+	[]Node{11},
+	[]Node{15, 16},
+	[]Node{12, 14, 15, 17},
+	[]Node{16},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
+// Nodes: 20, Edges: 57
+var g20 = Graph{
+	[]Node{4, 6, 9, 10, 15, 16, 18},
+	[]Node{3, 6, 7, 8, 12, 14, 16, 19},
+	[]Node{4, 9, 10, 14, 15, 16, 17, 18},
+	[]Node{7, 9, 10, 19},
+	[]Node{6, 10, 11, 17, 19},
+	[]Node{6, 8, 11, 18, 19},
+	[]Node{9, 15, 17, 19},
+	[]Node{12, 17, 18},
+	[]Node{11},
+	[]Node{15, 16},
+	[]Node{12, 14, 15, 17},
+	[]Node{16},
+	[]Node{19},
+	[]Node{18, 19},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+	[]Node{},
+}
+
 var testCases = []struct {
 	name   string
 	graph  Graph
@@ -40,5 +155,20 @@ func TestTopologicalSort(t *testing.T) {
 		if got, want := sorted, tc.sorted; reflect.DeepEqual(got, want) == false {
 			t.Errorf("\n\t got: %v\n\twant: %v\n", got, want)
 		}
+	}
+}
+
+func benchmarkTopologicalSort(g Graph, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		TopologicalSort(g)
+	}
+}
+
+func BenchmarkTopologicalSort(b *testing.B) {
+	graphs := []Graph{g10, g12, g14, g16, g18, g20}
+	for i := range graphs {
+		b.Run(fmt.Sprintf("_%d", len(graphs[i])), func(b *testing.B) {
+			benchmarkTopologicalSort(graphs[i], b)
+		})
 	}
 }
